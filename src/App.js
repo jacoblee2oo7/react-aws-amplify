0cc2a3943f67a2fb59cd19029fbb6bb676b9523d
jacoblee2oo7 <131315065+jacoblee2oo7@users.noreply.github.com>
2023-04-21 09:46:39 -0700

    my changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,56 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div class="parallax">
+        <div class="layer-1">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+        <div class="layer-2">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+        </div>
+        <div class="layer-3">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+          </header>
+        </div>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.js
+++ src/index.js
@@ -11,6 +11,33 @@ root.render(
   </React.StrictMode>
 );
 
+// Get the parallax container element
+const parallax = document.querySelector(".parallax");
+
+// Add an event listener to scroll the parallax layers
+parallax.addEventListener("scroll", function () {
+  // Get the scroll position of the parallax container
+  const scrollTop = parallax.scrollTop;
+
+  // Get the parallax layer elements
+  const layers = parallax.querySelectorAll(".layer");
+
+  // Loop through each layer and update its position
+  for (let i = 0; i < layers.length; i++) {
+    // Get the current layer element
+    const layer = layers[i];
+
+    // Get the depth of the layer (the z-index value)
+    const depth = layer.getAttribute("data-depth");
+
+    // Calculate the movement of the layer based on the depth and scroll position
+    const movement = -(scrollTop * depth);
+
+    // Apply a CSS transform to translate the layer vertically
+    layer.style.transform = `translate3d(0, ${movement}px, 0)`;
+  }
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
