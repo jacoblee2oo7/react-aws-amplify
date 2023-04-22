417f849a20fae8dacf5a973c1c05d9dc57ebcc5f
jacoblee2oo7 <131315065+jacoblee2oo7@users.noreply.github.com>
2023-04-21 18:28:48 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-              Edit <code>src/App.js</code> and save to reload. Updating test.
+              Edit 1 <code>src/App.js</code> and save to reload. Updating test.
             </p>
             <a
               className="App-link"
@@ -25,7 +25,7 @@ function App() {
           <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <p>
-                Edit <code>src/App.js</code> and save to reload.
+                Edit 2<code>src/App.js</code> and save to reload.
               </p>
               <a
                 className="App-link"
@@ -41,7 +41,7 @@ function App() {
           <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <p>
-                Edit <code>src/App.js</code> and save to reload.
+                Edit 3<code>src/App.js</code> and save to reload.
               </p>
               <a
                 className="App-link"
@@ -54,20 +54,6 @@ function App() {
           </header>
         </div>
       </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
